a9e16b31b74eca953a79444602bea1b9a22f5b65
Adam Nasrudin <adamnasrudin858@gmail.com>
2024-07-22 11:28:36 +0700

    add field status

--- app/models/shop.go
+++ app/models/shop.go
@@ -4,5 +4,6 @@ type Shop struct {
 	ID     uint64 `json:"id" gorm:"primary_key;auto_increment"`
 	UserID uint64 `json:"user_id" gorm:"not null"`
 	Name   string `json:"name" gorm:"not null;unique"`
+	Status string `json:"status" gorm:"not null"`
 	DefaultModel
 }
